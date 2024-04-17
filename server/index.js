6837d41fb60a8f39e1a25aa46f43a15002c42955
amaana259 <120198883+amaana259@users.noreply.github.com>
2024-04-17 20:11:02 +0500

    please man please

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from "dotenv";
 
+import userRoute from './routes/user.js';
+import productRoute from './routes/product.js';
+import chatRoute from './routes/chat.js';
+import orderRoute from './routes/order.js';
+import cartRoute from './routes/cart.js';
+import prodcartRoute from './routes/productsincart.js';
+import wishlistRoute from './routes/wishlist.js'
+import reviewRoute from './routes/reviews.js'
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+
 dotenv.config();
 
 const app = express()
@@ -31,6 +42,32 @@ try {
   } catch (err) {
   console.log(err);
 }
+// User api routing
+app.use("/api/user", userRoute);
+
+// Product api routing
+app.use("/api/product", productRoute);
+
+// Chat api routing
+app.use("/api/chat", chatRoute);
+
+// Cart api routing
+app.use("/api/cart", cartRoute);
+
+// Products in Cart api routing
+app.use("/api/prodcart", prodcartRoute);
+
+// Wishlist api routing
+app.use("/api/wishlist", wishlistRoute);
+
+// Orders api routing
+app.use("/api/orders", orderRoute);
+
+// Reviews api routing
+app.use("/api/review", reviewRoute);
+
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   try{
     res.json("Hello");
