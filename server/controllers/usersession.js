c38bc97d4f7d63239942b2b70fdccb7b35f2f6df
amaana259 <120198883+amaana259@users.noreply.github.com>
2024-04-26 17:58:19 +0500

    Update usersession.js

--- server/controllers/usersession.js
+++ server/controllers/usersession.js
@@ -21,14 +21,16 @@ export const createUserSession = TryCatch(async (req, res, next) => {
 export const deleteUserSession = TryCatch(async (req, res) => {
     try {
       const { userID } = req.body;
-  
+
       if (typeof userID !== 'string') {
         return res.status(400).json({ error: 'Invalid userID format' });
       }
-  
-      console.log('Deleting sessions for user:', userID);
-  
-      await userSession.deleteMany({ userID }); // Delete sessions matching userID
+
+      const modifiedUserID = userID.slice(1, -1);
+
+      console.log('Deleting sessions for user:', modifiedUserID);
+
+      await userSession.deleteMany({ userID: modifiedUserID });
       res.status(200).json({ message: 'User sessions deleted successfully' });
     } catch (error) {
       console.error('Error deleting user sessions:', error);
